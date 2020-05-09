b95745869e89e732e37c986d967910bfba6a3bb5
elpro <elprogramador0001@gmail.com>
2020-05-09 11:34:25 -0400

    Modifique mucho el WpfContrato y el daoContrato

--- BeLife/Controlador/DaoContrato.cs
+++ BeLife/Controlador/DaoContrato.cs
@@ -39,8 +39,8 @@ namespace Controlador
                     c.Plan = new DaoPlan().Read(dr[3].ToString());
                     c.FechaInicioVigencia = DateTime.Parse(dr[4].ToString());
                     c.FechaFinVigencia = DateTime.Parse(dr[5].ToString());
-                    c.Vigente = dr[6].ToString().Equals("1") ? true : false;
-                    c.DeclaracionSalud = dr[7].ToString().Equals("1") ? true : false;
+                    c.Vigente = bool.Parse(dr[6].ToString());
+                    c.DeclaracionSalud = bool.Parse(dr[7].ToString());
                     c.PrimaAnual = float.Parse(dr[8].ToString());
                     c.PrimaMensual= float.Parse(dr[9].ToString());
                     c.Observaciones = dr[10].ToString();
@@ -92,6 +92,82 @@ namespace Controlador
         }
 
 
+        public List<Contrato> ReadAllByNumeroContrato(string numero)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "sp_read_all_contrato_numero";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Connection = cone;
+                cmd.Parameters.Add("@numero", System.Data.SqlDbType.NVarChar, 14).Value = numero;
+                List<Contrato> lista = new List<Contrato>();
+                cone.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    Contrato c = new Contrato();
+                    c.Numero = dr[0].ToString();
+                    c.FechaCreacion = DateTime.Parse(dr[1].ToString());
+                    c.Cliente = new DaoCliente().Read(dr[2].ToString());
+                    c.Plan = new DaoPlan().Read(dr[3].ToString());
+                    c.FechaInicioVigencia = DateTime.Parse(dr[4].ToString());
+                    c.FechaFinVigencia = DateTime.Parse(dr[5].ToString());
+                    c.Vigente = dr[6].ToString().Equals("1") ? true : false;
+                    c.DeclaracionSalud = dr[7].ToString().Equals("1") ? true : false;
+                    c.PrimaAnual = float.Parse(dr[8].ToString());
+                    c.PrimaMensual = float.Parse(dr[9].ToString());
+                    c.Observaciones = dr[10].ToString();
+                    lista.Add(c);
+                }
+                cone.Close();
+                return lista;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+
+        public List<Contrato> ReadAllByRut(string rut)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "sp_read_all_contrato_rut";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Connection = cone;
+                cmd.Parameters.Add("@rut", System.Data.SqlDbType.NVarChar, 10).Value = rut;
+                List<Contrato> lista = new List<Contrato>();
+                cone.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    Contrato c = new Contrato();
+                    c.Numero = dr[0].ToString();
+                    c.FechaCreacion = DateTime.Parse(dr[1].ToString());
+                    c.Cliente = new DaoCliente().Read(dr[2].ToString());
+                    c.Plan = new DaoPlan().Read(dr[3].ToString());
+                    c.FechaInicioVigencia = DateTime.Parse(dr[4].ToString());
+                    c.FechaFinVigencia = DateTime.Parse(dr[5].ToString());
+                    c.Vigente = dr[6].ToString().Equals("1") ? true : false;
+                    c.DeclaracionSalud = dr[7].ToString().Equals("1") ? true : false;
+                    c.PrimaAnual = float.Parse(dr[8].ToString());
+                    c.PrimaMensual = float.Parse(dr[9].ToString());
+                    c.Observaciones = dr[10].ToString();
+                    lista.Add(c);
+                }
+                cone.Close();
+                return lista;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+
         public bool CREATE(Contrato c)
         {
             try
@@ -106,11 +182,11 @@ namespace Controlador
                 cmd.Parameters.Add("@plan", System.Data.SqlDbType.NVarChar, 5).Value = c.Plan.IdPlan;
                 cmd.Parameters.Add("@fechaivi", System.Data.SqlDbType.DateTime).Value = c.FechaInicioVigencia;
                 cmd.Parameters.Add("@fechatvi", System.Data.SqlDbType.DateTime).Value = c.FechaFinVigencia;
-                cmd.Parameters.Add("@vigencia", System.Data.SqlDbType.Bit).Value = c.Vigente;
+                cmd.Parameters.Add("@vigencia", System.Data.SqlDbType.Bit).Value = c.Vigente ? 1 : 0;
                 cmd.Parameters.Add("@salud", System.Data.SqlDbType.Bit).Value = c.DeclaracionSalud;
                 cmd.Parameters.Add("@primaanual", System.Data.SqlDbType.Float).Value = c.PrimaAnual;
                 cmd.Parameters.Add("@primamensual", System.Data.SqlDbType.Float).Value = c.PrimaMensual;
-                cmd.Parameters.Add("@obser", System.Data.SqlDbType.NVarChar).Value = c.Observaciones;
+                cmd.Parameters.Add("@obser", System.Data.SqlDbType.NVarChar,50).Value = c.Observaciones;
                 cone.Open();
                 int x = cmd.ExecuteNonQuery();
                 cone.Close();
@@ -132,7 +208,7 @@ namespace Controlador
             try
             {
                 SqlCommand cmd = new SqlCommand();
-                cmd.CommandText = "sp_actualizar_contrato";
+                cmd.CommandText = "sp_update_contrato";
                 cmd.CommandType = System.Data.CommandType.StoredProcedure;
                 cmd.Connection = cone;
                 cmd.Parameters.Add("@numero", System.Data.SqlDbType.NVarChar, 14).Value = c.Numero;
@@ -159,8 +235,34 @@ namespace Controlador
             {
                 return false;
             }
+        }
+
+
+        public bool DELETE(String numero)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "sp_delete_contrato";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Connection = cone;
+                cmd.Parameters.Add("@numero", System.Data.SqlDbType.NVarChar, 14).Value = numero;
+                cone.Open();
+                int x = cmd.ExecuteNonQuery();
+                cone.Close();
+                if (x > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
 
         }
+
 
     }
 }
--- BeLife/Vista/WpfContrato.xaml.cs
+++ BeLife/Vista/WpfContrato.xaml.cs
@@ -21,6 +21,11 @@ namespace Vista
     /// </summary>
     public partial class WpfContrato : Window
     {
+        //Para el campo PrimaAnual del Wpf
+        float prima = 0;
+        double recargo = 0;
+        float vpa = 0;
+
         public WpfContrato()
         {
             InitializeComponent();
@@ -32,6 +37,54 @@ namespace Vista
             lblFechaCreacion.Content = hoy.ToString("dd/MM/yyyy");
         }
 
+
+        private void limpiar()
+        {
+            txtNumero.Text = DateTime.Now.ToString("yyyyMMddHHmmss");
+            txtRut.Text = "";
+            lblNombre.Content = "Cliente";
+            cboPlan.SelectedIndex = -1;
+            dtpFechaInicioVigencia.Text = DateTime.Now.ToString("dd/MM/yyyy");
+            lblFechaFinVigencia.Content = 0;
+            lblPrimaAnual.Content = 0;
+            lblPrimaMensual.Content = 0;
+            txtObservaciones.Text = "";
+            lblVigencia.Content = "Vigencia";
+        }
+
+        public void LlenarContrato(Contrato c)
+        {
+            txtNumero.Text = c.Numero;
+            lblFechaCreacion.Content = c.FechaCreacion;
+            txtRut.Text = c.Cliente.Rut;
+            cboPlan.Text = c.Plan.ToString();
+            dtpFechaInicioVigencia.Text = c.FechaInicioVigencia.ToString("dd/MM/yyyy");
+            lblFechaFinVigencia.Content = c.FechaFinVigencia.ToString("dd/MM/yyyy");
+            if (c.Vigente)
+            {
+                lblVigencia.Content = "Contrato vigente";
+            }
+            else
+            {
+                lblVigencia.Content = "Contrato NO vigente";
+            }
+
+            if (c.DeclaracionSalud)
+            {
+                rbtSi.IsChecked=true;
+            }
+            else
+            {
+                rbtNo.IsChecked=true;
+            }
+
+            lblPrimaAnual.Content = c.PrimaAnual + " UF";
+            lblPrimaMensual.Content =c.PrimaMensual + " UF";
+            txtObservaciones.Text = c.Observaciones;
+
+        }
+
+
         private void CboPlan_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             try
@@ -39,11 +92,19 @@ namespace Vista
                 if (cboPlan.SelectedIndex == -1)
                 {
                     lblPoliza.Content = "Poliza";
+                    lblPrimaAnual.Content = 0;
+                    prima = 0;
+                    recargo = 0;
+                    vpa = 0;
                 }
                 else
                 {
+                    vpa -= prima;
                     Plan p = (Plan)cboPlan.SelectedItem;
                     lblPoliza.Content = p.PolizaActual;
+                    prima = p.PrimaBase;
+                    vpa += prima;
+                    lblPrimaAnual.Content = vpa;
                 }
             }
             catch (Exception ex)
@@ -51,6 +112,13 @@ namespace Vista
                 MessageBox.Show(ex.Message);
             }
         }
+
+
+        private void BtnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            limpiar();
+        }
+
 
         private void BtnComprobar_Click(object sender, RoutedEventArgs e)
         {
@@ -60,8 +128,14 @@ namespace Vista
                 Cliente clie = cli.Read(txtRut.Text);
                 if (clie != null)
                 {
+                    vpa -= (float)recargo;
                     txtRut.Text = clie.Rut;
                     lblNombre.Content = clie.Nombre + ' ' + clie.Apellido;
+                    Tarifador calculo = new Tarifador();
+                    calculo.cliente = clie;
+                    recargo = calculo.CalcularPrima();
+                    vpa += (float)recargo;
+                    lblPrimaAnual.Content = vpa;
                     MessageBox.Show("Cliente encontrado");
                 }
                 else
@@ -74,5 +148,93 @@ namespace Vista
                 MessageBox.Show(ex.Message);
             }
         }
+
+
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Contrato c = new Contrato();
+                DaoCliente dc = new DaoCliente();
+                Cliente cli = dc.Read(txtRut.Text);
+
+                c.Numero = txtNumero.Text;
+                c.FechaCreacion = DateTime.Now;
+
+                if (cli != null)
+                {
+                    c.Cliente = cli;
+                }
+                else
+                {
+                    throw new Exception("El rut del cliente no esta registrado");
+                }
+
+                if (cboPlan.SelectedIndex >= 0)
+                {
+                    c.Plan = (Plan)cboPlan.SelectedItem;
+                }
+                else
+                {
+                    throw new Exception("Seleccione un plan");
+                }
+
+                c.FechaInicioVigencia = (DateTime)dtpFechaInicioVigencia.SelectedDate;
+                c.FechaFinVigencia = c.FechaInicioVigencia.AddYears(1);
+                
+                c.Vigente = true;
+
+                if (rbtSi.IsChecked==true)
+                {
+                    c.DeclaracionSalud = true;
+                }
+                else
+                {
+                    c.DeclaracionSalud = false;
+                }
+
+                c.Observaciones = txtObservaciones.Text;
+                c.PrimaAnual = (float)Math.Round(c.ValorPrimalAnual(), 4);
+                c.PrimaMensual = (float)Math.Round((c.PrimaAnual / 12), 4);
+
+                DaoContrato crea = new DaoContrato();
+                bool resp = crea.CREATE(c);
+                MessageBox.Show(resp ? "Contrato guardado" : "No Guardo");
+                limpiar();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void BtnRead_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DaoContrato dc = new DaoContrato();
+                Contrato c = dc.Read(txtNumero.Text);
+                if (c != null)
+                {
+                    LlenarContrato(c);
+                }
+                else
+                {
+                    txtNumero.Focus();
+                    throw new Exception("Error, Ese numero de contrato no existe");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
     }
 }
