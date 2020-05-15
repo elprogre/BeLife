3c13313ed511677dfe58e7ae6cec66558d467cb2
elpro <elprogramador0001@gmail.com>
2020-05-15 19:31:16 -0400

    Agregue tipoEmpresa,modalidadServicio,actividad empresa,Cliente y la clase CommonBC(sirve para resumir) agregue algunos metodos a esas clases tambien, por ultimo agregue la vista cliente e inicio (imcompletas)

--- OnBreak - EF/Biblioteca.Negocio/ModalidadServicio.cs
+++ OnBreak - EF/Biblioteca.Negocio/ModalidadServicio.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Biblioteca.DALC;
 
 namespace Biblioteca.Negocio
 {
-    class ModalidadServicio
+    public class ModalidadServicio
     {
+        public string IdModalidad { get; set; }
+        public int IdTipoEvento { get; set; }
+        public string Nombre { get; set; }
+        public double ValorBase { get; set; }
+        public int PersonalBase { get; set; }
+
+        public ModalidadServicio()
+        {
+
+        }
+
+        OnBreakEntities bdd = new OnBreakEntities();
+
+
+        //falta verificar si esto esta bien///
+        public bool Read()
+        {
+            try
+            {
+                DALC.ModalidadServicio mod =
+                bdd.ModalidadServicio.First(m => m.IdModalidad == IdModalidad);
+                this.IdModalidad = mod.IdModalidad;
+                this.IdTipoEvento = mod.IdTipoEvento;
+                this.Nombre = mod.Nombre;
+                this.ValorBase = mod.ValorBase;
+                this.PersonalBase = mod.PersonalBase;
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
     }
 }
